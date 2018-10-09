1f589192ecab65bade99b1593747c40c7101d85b
Xoab <35434658+xoabperez@users.noreply.github.com>
2018-10-09 12:28:57 -0500

    updated linear elastic module to compare different formulations

--- le.py
+++ le.py
@@ -14,17 +14,34 @@ def vm(u, mu, lam, W):
     von_Mises = sqrt(3./2*inner(s, s))
     return project(von_Mises, W)
 
-def varprob(V, W, mbc, phi, mu, lam, omega):
+def varprob(V, W, T, n, mbc, phi, mu, lam, omega):
     # Define variational problem
-    nu = lam/(2*(lam+mu))
-    
     u = TrialFunction(V)
     v = TestFunction(V)
-    F = -inner(sigma(u, mu, lam), E(v))*dx - dot(grad(omega*phi), v)*dx 
+    F = -inner(sigma(u, mu, lam), E(v))*dx - dot(grad(omega*phi), v)*dx + dot(T*n,v)*ds
     a, L = lhs(F), rhs(F)
     
     # Compute solution
     u = Function(V)
     solve(a == L, u, mbc)
-    vmstress = vm(u, mu, lam, W)
-    return vmstress
+    return u
+
+
+def sigma1(u, mu, lam, omega, phi):
+    return 2*mu*(E(u)-omega*phi*Identity(2))+lam*(tr(E(u))-2*omega*phi)*Identity(2)
+
+def vm1(u, mu, lam, omega, phi, W):
+    s = sigma1(u, mu, lam, omega, phi) - (1./3)*tr(sigma1(u, mu, lam, omega, phi))*Identity(2)  # deviatoric stress
+    von_Mises = sqrt(3./2*inner(s, s))
+    return project(von_Mises, W)
+
+def varprob1(V, W, T, n, mbc, phi, mu, lam, omega):
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    F = -inner(sigma1(u, mu, lam, omega, phi), E(v))*dx + dot(T*n,v)*ds
+    a, L = lhs(F), rhs(F)
+    
+    # Compute solution
+    u = Function(V)
+    solve(a == L, u, mbc)
+    return u

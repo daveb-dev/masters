e29c5e69d83069d228feff560b0715c11dc60d20
Xoab Perez <35434658+xoabperez@users.noreply.github.com>
2018-10-15 08:35:04 -0500

    Updated matlab code for boundaries to be finer; working on rat05mesh now

--- rat-data/rat05/rat05mshr.m
+++ rat-data/rat05/rat05mshr.m
@@ -6,15 +6,16 @@ close all;
 % comment: 
 % (x_skull, y_skull) are coordinates of the domain boundary
 % (x_tumor, y_tumor) are coordinates of the tumor ROI
-% Note, the list shall not contain repeated points.
-load('rat3skull.mat')
-load('rat3tumor.mat')
+% Note, THE LIST SHALL NOT CONTAIN REPEATED POINTS
+load('rat05skull.mat')
+load('rat05tumor.mat')
 global x_skull y_skull x_tumor y_tumor
-x_skull = rat3sbound(1:end-1,1);
-y_skull = rat3sbound(1:end-1,2);
-x_tumor = rat3tbound(1:end-1,1);
-y_tumor = rat3tbound(1:end-1,2);
-output_filename = 'rat3gmsh.geo';
+x_skull = rat05skull(1:end-1,1);
+y_skull = rat05skull(1:end-1,2);
+x_tumor = rat05tumor(1:end-1,1);
+y_tumor = rat05tumor(1:end-1,2);
+output_filename = 'rat05gmsh.geo';
 util_generateGmshGeo(output_filename,20,10);
-!~/gmsh2/gmsh rat3gmsh.geo -2 -o rat3gmsh.msh
-!~/gmsh2/gmsh rat3gmsh.msh
+!~/gmsh2/gmsh rat05gmsh.geo -2 -o rat05gmsh.msh
+!~/gmsh2/gmsh rat05gmsh.msh
+!dolfin-convert rat05gmsh.msh rat05gmsh.xml

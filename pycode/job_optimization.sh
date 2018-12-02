c8b5a410e2bacceb37e2dc0855e67e3bf93447b8
Xoab <35434658+xoabperez@users.noreply.github.com>
2018-12-01 19:28:54 -0600

    installed h5py, going to try and run models for days after 2 starting from parameters estimated at day 2

--- pycode/job_optimization.sh
+++ pycode/job_optimization.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-D0=.5
-gammaD=.5
-k0=2.
-beta=.1
-
 for lin_hyp in 0 1
 do
+    if [ $lin_hyp -eq 0 ]
+    then
+	gammaD=0.212689163148
+	D0=0.831665484255
+	beta=0.01
+	k0=1.
+    else
+	gammaD=1.9918771142
+	D0=1.01849141246
+	beta=0.034372054592
+	k0=1.
+    fi
+    
     for day in 4 5 6 9
     do
 	printf "lin_hyp = %i \n day = %i \n D0 = %f, gammaD = %f, k0 = %f, beta = %f \n" $lin_hyp $day $D0 $gammaD $k0 $beta 
--- pycode/optimization_Rat05.py
+++ pycode/optimization_Rat05.py
@@ -4,7 +4,7 @@ from numpy import fliplr, linspace, inf
 from os.path import join as osjoin
 from scipy.io import loadmat as sc_io_loadmat
 from scipy.interpolate import RegularGridInterpolator
-
+import h5py
 set_log_level(ERROR) 
 
 class InterpolatedParameter(Expression):
@@ -207,12 +207,12 @@ def optimize(dbg=False):
     
     # upper and lower bound for the parameter field
     D_lb = 0.
-    D_ub = 3.
+    D_ub = 5.
     k_lb, k_ub = Function(V,annotate=False), Function(V,annotate=False)
     k_lb.vector()[:] = 0.
-    k_ub.vector()[:] = 5.
+    k_ub.vector()[:] = 1.
     gD_lb = .01
-    gD_ub = 2.
+    gD_ub = 1.
     beta_lb = 0.01
     beta_ub = 1.
     bnds = [[D_lb, gD_lb, k_lb, beta_lb],[D_ub, gD_ub, k_ub, beta_ub]]
@@ -253,8 +253,10 @@ if __name__ == "__main__":
     input_dir  = "../rat-data/rat05/"
     if lin_hyp == 0:
         output_dir = './output/rat05le_day'+str(day)
+        fl = h5py.File("./output/rat05le_day2/notime.h5", "r")
     else:
         output_dir = './output/rat05he_day'+str(day)
+        fl = h5py.File("./output/rat05he_day2/notime.h5", "r")
 
     # Prepare output file
     f_timeseries = XDMFFile(osjoin(output_dir,'timeseries.xdmf'))
@@ -330,6 +332,14 @@ if __name__ == "__main__":
     gammaD = Constant(gammaD)     # initial guess of gamma_D
     k      = project(Constant(k0),V,annotate=False)     # growth rate initial guess
     beta   = Constant(beta)
+
+    # Choose the first time step
+    veck = fl["/VisualisationVector/0"]
+    # Scalar FunctionSpace Q is required for mapping vertices to dofs 
+    Q = FunctionSpace(mesh, 'CG', 1)
+    v2d = vertex_to_dof_map(Q)
+    # Now map vertexfunction to the V function space
+    k.vector()[v2d] = veck[:]
     
     # Optimization module
     [D0, gammaD, k, beta] = optimize() # optimize the k field, gammaD, and D0 using the adjoint method provided by adjoint_dolfin

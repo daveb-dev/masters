66629ad7f0233732737075b06582759d79d4181d
Xoab Perez <35434658+xoabperez@users.noreply.github.com>
2019-02-15 16:33:06 -0600

    change to script for running opt

--- pycode/run_all_opt.sh
+++ pycode/run_all_opt.sh
@@ -4,6 +4,6 @@ for rat_num in 01 02 05 06 09 12
 do
     for day_index in 1 2 3
     do
-	python all_rats_optimization.py $rat_num $day_index
+	python2.7 all_rats_optimization.py $rat_num $day_index
     done
 done
